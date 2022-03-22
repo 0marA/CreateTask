966ab2ab1d78e063a4e36fbb0d2f0af4d91e1a9f
0marA <theengineermachine@gmail.com>
2022-03-22 11:25:46 -0500

    Switch to Wordle. Added some words

---
+++ Wordle.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Wordle {
+    private ArrayList<String> words;
+    
+    public void addWords() {
+        words = new ArrayList<String>();
+        words.add("laptop");
+        words.add("abacus");
+        words.add("baboon");
+        words.add("cabana");
+        words.add("fabric");
+        words.add("rabbit");
+    }
+}
