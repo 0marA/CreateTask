595244a73847b85a5f05fd8df2f75f1307226265
0marA <theengineermachine@gmail.com>
2022-03-24 18:44:39 -0500

    It works!

--- Wardle.java
+++ Wardle.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class Wardle {
     public static ArrayList<String> words;
+    public static ArrayList<Character> correctLetters = new ArrayList<Character>();
     public static Scanner scanner = new Scanner(System.in);
+    public boolean won = false;
+    public static int numOfAttempts = 0;
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_RESET = "\u001B[0m";
@@ -12,18 +15,29 @@ public class Wardle {
     public static void main(String[] args) {
         gameIntroduction();
         String wordToGuess = pickRandomWord();
-        String playerGuess = readInput();
-        //checkGuess(wordToGuess, readInput());
+        for (int i = 0; i < 6; i++) {
+            correctLetters.clear();
+            checkGuess(wordToGuess, readInput());
+            if (correctLetters.size() == 6) break;
+        }
+        if (correctLetters.size() == 6) System.out.println("Correct!");
+        else System.out.println("Sorry, but you lose :(");
+
     }
+
+    /**
+     * Reads a word from the player and will only accept it if it is six letters
+     * @return A six letter word from the player
+     */
 
     public static String readInput() {
         boolean stop = false;
         String word = "";
+        numOfAttempts++;
         while (!stop) {
-            System.out.print("Enter a six letter word: ");
+            System.out.print(numOfAttempts + ". Enter a six letter word: ");
             word = scanner.next();
             if (word.length() == 6) {
-                scanner.close();
                 return word.toLowerCase();
             } else {
                 System.out.println("Your word has to be six letters!");
@@ -66,20 +80,23 @@ public class Wardle {
         ArrayList<Character> wordChars = new ArrayList<Character>();
         ArrayList<Character> playerGuessChars = new ArrayList<Character>();
 
-        //Splits the String of the word to guess into an ArrayList of characters
+        // Splits the String of the word to guess into an ArrayList of characters
         for (int i = 0; i < 6; i++) {
             wordChars.add(word.charAt(i));
         }
 
-        //Splits the String of the players guess into an ArrayList of characters
+        // Splits the String of the players guess into an ArrayList of characters
         for (int j = 0; j < 6; j++) {
             playerGuessChars.add(playerGuess.charAt(j));
         }
 
         
         for (int k = 0; k < 6; k++) {
+            // Correct letter and in the correct spot
             if (wordChars.get(k).equals(playerGuessChars.get(k))) printOutput(ANSI_GREEN, playerGuessChars.get(k));
+            // Correct letter, but in the wrong spot
             else if (wordChars.contains(playerGuessChars.get(k))) printOutput(ANSI_YELLOW, playerGuessChars.get(k));
+            // Not a letter in the word
             else printOutput(ANSI_RESET, playerGuessChars.get(k));
         }
 
@@ -93,6 +110,7 @@ public class Wardle {
      */
 
     public static void printOutput(String printColor, char letter) {
+        if (printColor.equals(ANSI_GREEN)) correctLetters.add(letter);
         System.out.print(printColor + letter + ANSI_RESET);
     }
 
